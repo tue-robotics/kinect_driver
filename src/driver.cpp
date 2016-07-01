486cba8a78a5478e1ff5ff35aa7b7274aeca7ef5
Max Baeten <m.j.j.baeten@student.tue.nl>
2016-07-01 13:50:02 +0200

    commit for Rein, error log for kinect

--- src/driver.cpp
+++ src/driver.cpp
@@ -8,6 +8,7 @@
 
 #include <ros/init.h>
 #include <ros/node_handle.h>
+#include <ros/console.h>
 
 #include <kinect_driver/SetSettings.h>
 
@@ -31,7 +32,7 @@ void depth_cb(freenect_device *dev, void *v_depth, uint32_t timestamp)
             d = depth[i];// / 1000;
         else
             d = 0;
-        //            std::cout << d << std::endl;
+        //            ROS_ERROR_STREAM(d);
         depth_image.at<float>(i) = d;
     }
 
@@ -99,7 +100,7 @@ int main(int argc, char **argv)
     // Initialize freenect
     if (freenect_init(&f_ctx, NULL) < 0)
     {
-        std::cout << "freenect_init() failed" << std::endl;
+        ROS_ERROR_STREAM("freenect_init() failed");
         return 1;
     }
 
@@ -107,19 +108,19 @@ int main(int argc, char **argv)
     freenect_select_subdevices(f_ctx, (freenect_device_flags)(FREENECT_DEVICE_CAMERA));
 
     int nr_devices = freenect_num_devices (f_ctx);
-    std::cout << "[KINECT DRIVER] Number of devices found: " << nr_devices << std::endl;
+    ROS_ERROR_STREAM("[KINECT DRIVER] Number of devices found: " << nr_devices);
 
     if (nr_devices < 1) {
         freenect_shutdown(f_ctx);
         return 1;
     }
 
-    std::cout << "[KINECT DRIVER] Opening device..." << std::endl;
+    ROS_ERROR_STREAM("[KINECT DRIVER] Opening device...");
 
     int user_device_number = 0;
     if (freenect_open_device(f_ctx, &f_dev, user_device_number) < 0)
     {
-        std::cout << "[KINECT DIVER] Could not open device" << std::endl;
+        ROS_ERROR_STREAM("[KINECT DIVER] Could not open device");
         freenect_shutdown(f_ctx);
         return 1;
     }
@@ -144,7 +145,7 @@ int main(int argc, char **argv)
     freenect_start_depth(f_dev);
     freenect_start_video(f_dev);
 
-    std::cout << "[KINECT DRIVER] Listening to video stream" << std::endl;
+    ROS_ERROR_STREAM("[KINECT DRIVER] Listening to video stream");
 
     // - - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -156,7 +157,7 @@ int main(int argc, char **argv)
     cam_model.setOpticalTranslation(0, 0);
     cam_model.setOpticalCenter(cx, cy);
 
-    std::cout << "[KINECT DRIVER] Up and running" << std::endl;
+    ROS_ERROR_STREAM("[KINECT DRIVER] Up and running");
 
     while (ros::ok())
     {
@@ -172,13 +173,13 @@ int main(int argc, char **argv)
         } 
         catch (boost::thread_resource_error const &)
         {
-            std::cout << "[KINECT DRIVER] boost::thread_resource_error catch, continue ..." << std::endl;
-            continue;
+            ROS_ERROR_STREAM("[KINECT DRIVER] boost::thread_resource_error catch, continue ...");
+            return 1;
         }
 
         if (res < 0 && res != -10)
         {
-            std::cout << "[KINECT DRIVER] Error " << res << " received from libusb - aborting." << std::endl;
+            ROS_ERROR_STREAM("[KINECT DRIVER] Error " << res << " received from libusb - aborting.");
             return 1;
         }
 
@@ -192,7 +193,7 @@ int main(int argc, char **argv)
 
     // - - - - - - - - - - - - - - - - - - - - - - - -
 
-    std::cout << "[KINECT DRIVER] Driver stopped" << std::endl;
+    ROS_ERROR_STREAM("[KINECT DRIVER] Driver stopped");
 
     freenect_stop_depth(f_dev);
     freenect_stop_video(f_dev);
